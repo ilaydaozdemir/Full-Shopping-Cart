786ce8208f9a7e192f4a681283d57e176e798433
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2021-01-13 22:40:23 +0300

    header-footer

--- redux-shopping-cart/src/App.js
+++ redux-shopping-cart/src/App.js
@@ -1,5 +1,14 @@
+import React from 'react';
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='grid-container'>
+      <header>
+        <a href='/'>React Shopping Cart</a>
+      </header>
+      <main>Product List</main>
+      <footer>All right is reserved.</footer>
+    </div>
+  );
 }
 
 export default App;
