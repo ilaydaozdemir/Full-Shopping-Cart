3a47921c094bc3ee8fbad800c4220c065931620b
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2021-01-13 23:27:55 +0300

    structure

--- redux-shopping-cart/src/App.js
+++ redux-shopping-cart/src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
-function App() {
-  return (
-    <div className='grid-container'>
-      <header>
-        <a href='/'>React Shopping Cart</a>
-      </header>
-      <main>Product List</main>
-      <footer>All right is reserved.</footer>
-    </div>
-  );
+import data from './data.json';
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: '',
+      sort: '',
+    };
+  }
+  render() {
+    return (
+      <div className='grid-container'>
+        <header>
+          <a href='/'>React Shopping Cart</a>
+        </header>
+        <main>
+          <div className='content'>
+            <div className='main'>Products</div>
+            <div className='sidebar'>Cart Items</div>
+          </div>
+        </main>
+        <footer>All right is reserved.</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
